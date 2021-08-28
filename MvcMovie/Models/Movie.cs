1586355ad7e098bfee7fa1ecb7c929b8715437b9
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2021-08-28 15:11:24 -0300

    first commit

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Display(Name = "Título")]
+        public string Title { get; set; }
+
+        [Display(Name = "Data de Lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Gênero")]
+        public string Genre { get; set; }
+
+        [Display(Name = "Preço")]
+        public decimal Price { get; set; }
+    }
+}
